f954c4e392e31a6c47ea0015c852b5feff718698
muhthar <muhammedmuhthar11@gmail.com>
2022-10-10 15:53:11 +0530

    push

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const state={
 
 module.exports.connect= function(done){
    // const url ='mongodb://localhost:27017'
-    const url ="mongodb+srv://muhthar:muhthar@cluster0.c0pyjlk.mongodb.net/?retryWrites=true&w=majority"
+    const url ="mongodb+srv://muhthar:muhthar@cluster0.c0pyjlk.mongodb.net/ecommerce?retryWrites=true&w=majority"
     const dbname='ecommerce'
 
     mongoClient.connect(url,(err,data)=>{
